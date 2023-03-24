c5d2b4e8eea991c2b160022f513685b77359d6d9
vcjuliocesar <vcjuliocesar@outlook.com>
2023-03-24 00:32:37 -0600

    fizzbuzz.py modified

--- python/fizzbuzz/fizzbuzz.py
+++ python/fizzbuzz/fizzbuzz.py
@@ -1,12 +1,19 @@
+class FizzBuzz:
+    def convert(self,number):
+
+        result = ""
+        
+        if number % 3 == 0: 
+            result += "Fizz" 
+
+        if number % 5 == 0:
+            result += "Buzz"
+        
+        return result or number
+
 # range of numbers
 for i in range(1,100):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    elif i % 5 == 0:
-        print("Buzz")
-    else:
-        print(i)
+    print(FizzBuzz().convert(i))
+
 
      
